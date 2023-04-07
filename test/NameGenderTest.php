99e3d491a1a696887c37a4a8cea0cb67727642c7
Chri704r <christinepiilmann8@gmail.com>
2023-04-07 12:21:42 +0200

    beginning

---
+++ test/NameGenderTest.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'src/FakeInfo.php';
+
+use PHPUnit\Framework\TestCase;
+
+class NameGenderTest extends TestCase {
+
+    public function setUp(): void {
+		$this->Fakeinfo = new FakeInfo();
+	}
+
+	public function tearDown(): void {
+		unset($this->Fakeinfo);
+	}
+
+    public function testIsArray(){
+        $output = $this->FakeInfo->getFullNameAndGender();
+        $this->assertIsArray($output);
+    }
+
+}
