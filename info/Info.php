f81b7a1db921719c415c576e372b28284b89a19b
Chri704r <christinepiilmann8@gmail.com>
2023-04-07 23:11:00 +0200

    info php changed

--- info/Info.php
+++ info/Info.php
@@ -2,7 +2,7 @@
 
 class Info
 {
-    public const HOST = 'localhost';
+    public const HOST = 'localhost:8889';
     public const DB_NAME = 'addresses';
     public const USER = 'root';
     public const PASSWORD = 'root';
