61c404a8ee30dfb3528b2064d8f1d00a37448c2a
Chri704r <christinepiilmann8@gmail.com>
2023-04-07 18:48:09 +0200

    tests added

---
+++ test/cprTest.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'src/FakeInfo.php';
+
+use PHPUnit\Framework\TestCase;
+
+class cprTest extends TestCase 
+{
+
+    public function setUp(): void {
+		$this->fakeInfo = new FakeInfo();
+	}
+
+    public function tearDown(): void {
+		unset($this->fakeinfo);
+	}
+
+    public function testIsString(){
+        $result = $this->fakeInfo->getCpr();
+        $this->assertIsString($result, 'The expected result is a string');
+    }
+
+    public function testCprHasLength10(){
+        $expectedLength = 10;
+        $result = $this->fakeInfo->getCpr();
+        $this->assertEquals($expectedLength, strlen($result), "The expected result is length of {$expectedLength}");
+    }
+
+    public function testCprDateDay(){
+        $maxNumer = 31;
+        $result = substr($this->fakeInfo->getCpr(), 0, 2);
+        $this->assertLessThanOrEqual($maxNumer, $result, "The expected result is lower than {$maxNumer}");
+    }
+
+    public function testCprDateMonth(){
+        $maxNumer = 12;
+        $result = substr($this->fakeInfo->getCpr(), 2, 2);
+        $this->assertLessThanOrEqual($maxNumer, $result, "The expected result is lower than {$maxNumer}");
+    }
+
+    public function testCprDateYear(){
+        $maxNumer = 99;
+        $result = substr($this->fakeInfo->getCpr(), 4, 2);
+        $this->assertLessThanOrEqual($maxNumer, $result, "The expected result is lower than {$maxNumer}");
+    }
+
+}
