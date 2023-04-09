49d36baa34282ea85dff6c0e6c2a11605a2f994e
Chri704r <christinepiilmann8@gmail.com>
2023-04-09 14:50:47 +0200

    test about negative numbers added

--- test/cprTest.php
+++ test/cprTest.php
@@ -32,22 +32,40 @@ class cprTest extends TestCase
         $this->assertMatchesRegularExpression($regularExpresion, $result, "The expected result only contains digits");
     }
 
-    public function testCprDateDay(){
+    public function testCprDateDayIsGreaterOrEqual1(){
+        $lowestNumber = 1;
+        $result = substr($this->fakeInfo->getCpr(), 0, 2);
+        $this->assertGreaterThanOrEqual($lowestNumber, $result, "The expected result is greater than {$lowestNumber}");
+    }
+
+    public function testCprDateDayIsUnder31(){
         $maxNumer = 31;
         $result = substr($this->fakeInfo->getCpr(), 0, 2);
         $this->assertLessThanOrEqual($maxNumer, $result, "The expected result is lower than {$maxNumer}");
     }
 
-    public function testCprDateMonth(){
+    public function testCprDateMonthIsGreaterOrEqual1(){
+        $lowestNumber = 1;
+        $result = substr($this->fakeInfo->getCpr(), 2, 2);
+        $this->assertGreaterThanOrEqual($lowestNumber, $result, "The expected result is greater than {$lowestNumber}");
+    }
+
+    public function testCprDateMonthIsUnder12(){
         $maxNumer = 12;
         $result = substr($this->fakeInfo->getCpr(), 2, 2);
         $this->assertLessThanOrEqual($maxNumer, $result, "The expected result is lower than {$maxNumer}");
     }
 
-    public function testCprDateYear(){
+    public function testCprDateYearIsUnder99(){
         $maxNumer = 99;
         $result = substr($this->fakeInfo->getCpr(), 4, 2);
         $this->assertLessThanOrEqual($maxNumer, $result, "The expected result is lower than {$maxNumer}");
+    }
+
+    public function testCprLastDigitIsNotZero(){
+        $regex = '/[1-9]/';
+        $result = substr($this->fakeInfo->getCpr(), 9);
+        $this->assertMatchesRegularExpression($regex, $result, 'Expected result to be a number between 1 and 9');
     }
 
 }
