502a47fe89a4ff4d354ba9ea783c62e612a03b40
Chri704r <christinepiilmann8@gmail.com>
2023-04-10 12:05:16 +0200

    test gender

--- test/NameGenderTest.php
+++ test/NameGenderTest.php
@@ -6,7 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 class nameGenderTest extends TestCase 
 {
-
     public function setUp(): void {
 		$this->fakeInfo = new FakeInfo();
 	}
@@ -20,11 +19,11 @@ class nameGenderTest extends TestCase
         $this->assertIsArray($array, 'The expected result is an array');
     }
 
-    public function testIsSame(){
-        $array1 = $this->fakeInfo->getFullNameAndGender();
-        $array2 = $this->fakeInfo->getFullNameAndGender();
-        $this->assertSame($array1, $array2, 'The expected result is the same');
-    }
+    public function testArrayLengthIs3(){
+        $expectedLength = 3;
+        $array = $this->fakeInfo->getFullNameAndGender();
+        $this->assertCount($expectedLength, $array, "The expected result is array has a length of {$expectedLength}");
+    } 
 
     public function testArrayHasKeyFirstname(){
         $value = 'firstName';
@@ -48,7 +47,7 @@ class nameGenderTest extends TestCase
         $value = $this->fakeInfo->getFullNameAndGender()['firstName'];
         $this->assertIsString($value, 'The expected result is firstName is a string');
     }
-    
+
     public function testLastnameIsAString(){
         $value = $this->fakeInfo->getFullNameAndGender()['lastName'];
         $this->assertIsString($value, 'The expected result is lastName is a string');
@@ -59,10 +58,21 @@ class nameGenderTest extends TestCase
         $this->assertIsString($value, 'The expected result is gender is a string');
     }
 
-    public function testArrayLengthIs3(){
-        $expectedLength = 3;
-        $array = $this->fakeInfo->getFullNameAndGender();
-        $this->assertCount($expectedLength, $array, "The expected result is array has a length of {$expectedLength}");
-    } 
+    public function testIsSame(){
+        $array1 = $this->fakeInfo->getFullNameAndGender();
+        $array2 = $this->fakeInfo->getFullNameAndGender();
+        $this->assertSame($array1, $array2, 'The expected result is the same');
+    }
 
+   /*  public function testGenderIsFemale(){
+        $gender = $this->fakeInfo->getFullNameAndGender()["gender"];
+        $expectedResult = 'female';
+        $this->assertSame($gender, $expectedResult, "The expected result is true/female");
+    }
+
+    public function testGenderIsMale(){
+        $gender = $this->fakeInfo->getFullNameAndGender()["gender"];
+        $expectedResult = 'male';
+        $this->assertSame($gender, $expectedResult, "The expected result is true/male");
+    } */
 }
