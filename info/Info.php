2354fe3c33711834cd59a6544dc75938888467c7
SasiaDagnoli <sasi0026@stud.kea.dk>
2023-04-12 14:01:22 +0200

    CiTest file added

--- info/Info.php
+++ info/Info.php
@@ -2,7 +2,7 @@
 
 class Info
 {
-    public const HOST = 'localhost:8889';
+    public const HOST = 'localhost';
     public const DB_NAME = 'addresses';
     public const USER = 'root';
     public const PASSWORD = 'root';
