3275ae237db0dbacdeaea34ab8640abccccad1e6
Anna <anna355y@stud.kea.dk>
2023-04-12 08:31:27 +0200

    add comments to phone number test

--- test/PhoneNumberTest.php
+++ test/PhoneNumberTest.php
@@ -2,6 +2,7 @@
 
 require_once 'src/FakeInfo.php';
 
+
 use PHPUnit\Framework\TestCase;
 
 class PhoneNumberTest extends TestCase
@@ -13,42 +14,31 @@ class PhoneNumberTest extends TestCase
         $this->fakeInfo = new FakeInfo();
     }
 
+    // Assert that getPhoneNumber returns a string
     public function testPhoneNumberIsString()
     {
         $this->assertIsString($this->fakeInfo->getPhoneNumber());
     }
 
+    // Assert getPhoneNumber returns exactly 8 numbers
     public function testPhoneNumberHasCorrectFormat()
     {
         $phoneNumber = $this->fakeInfo->getPhoneNumber();
-        $this->assertMatchesRegularExpression("/^\d{8}$/", $phoneNumber);
+        $this->assertMatchesRegularExpression("/^[0-9]{1,8}$/", $phoneNumber);
     }
 
-
-    // This test fails... (Failed asserting that '53000459' is not equal to '53000459'.
-    public function testPhoneNumberIsUnique()
+    // Assert prefix is the first number in the phone number
+    public function testPhoneNumberPrefixIsFirst()
     {
-        $phoneNumber1 = $this->fakeInfo->getPhoneNumber();
-        $phoneNumber2 = $this->fakeInfo->getPhoneNumber();
+        $phoneNumber = $this->fakeInfo->getPhoneNumber();
+        $phonePrefixes = FakeInfo::PHONE_PREFIXES;
 
-        $this->assertNotEquals($phoneNumber1, $phoneNumber2);
-    }
-
-    // This also fails
-    // testing 1000 phone numbers -> adds them to array,
-    // then using 'array_unique' to remove dublicates -> asserting that the number of unique phone numbers is aqual to number of tests
-    public function testPhoneNumberIsUnique2()
-    {
-        $phoneNumbers = [];
-        $numTests = 1000;
-
-        for ($i = 0; $i < $numTests; $i++) {
-            $phoneNumbers[] = $this->fakeInfo->getPhoneNumber();
+        $foundPrefix = false;
+        foreach ($phonePrefixes as $prefix) {
+            if (str_starts_with($phoneNumber, $prefix)) {
+                $foundPrefix = true;
+                break;
+            }
         }
-
-        $uniqueNumbers = array_unique($phoneNumbers);
-        $numUnique = count($uniqueNumbers);
-
-        $this->assertEquals($numTests, $numUnique);
     }
 }
