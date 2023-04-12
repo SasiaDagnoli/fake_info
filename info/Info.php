fc7a47678d17025a113178c4d01869669f64f430
Nanna Lea <nannalea@MacBook-Pro-tilhrende-Nanna-4.local>
2023-04-12 14:59:04 +0200

    Added Info.php options for Windows/Mac.

--- info/Info.php
+++ info/Info.php
@@ -1,9 +1,21 @@
 <?php
 
+### WINDOWS ###
+
 class Info
 {
     public const HOST = 'localhost';
     public const DB_NAME = 'addresses';
     public const USER = 'root';
-    public const PASSWORD = 'root';
+    public const PASSWORD = '';
 }
+
+### MAC ####
+
+// class Info
+// {
+//     public const HOST = 'localhost:8889';
+//     public const DB_NAME = 'addresses';
+//     public const USER = 'root';
+//     public const PASSWORD = 'root';
+// }
