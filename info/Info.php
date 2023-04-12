e2db6cf68bceeb5ebbd68967c5e4d2b21d779310
Nanna Lea <nannalea@MacBook-Pro-tilhrende-Nanna-4.local>
2023-04-12 15:58:18 +0200

    Added Town_Name test.

--- info/Info.php
+++ info/Info.php
@@ -2,20 +2,20 @@
 
 ### WINDOWS ###
 
-class Info
-{
-    public const HOST = 'localhost';
-    public const DB_NAME = 'addresses';
-    public const USER = 'root';
-    public const PASSWORD = '';
-}
+// class Info
+// {
+//     public const HOST = 'localhost';
+//     public const DB_NAME = 'addresses';
+//     public const USER = 'root';
+//     public const PASSWORD = '';
+// }
 
 ### MAC ####
 
-// class Info
-// {
-//     public const HOST = 'localhost:8889';
-//     public const DB_NAME = 'addresses';
-//     public const USER = 'root';
-//     public const PASSWORD = 'root';
-// }
+class Info
+{
+    public const HOST = 'localhost:8889';
+    public const DB_NAME = 'addresses';
+    public const USER = 'root';
+    public const PASSWORD = 'root';
+}
