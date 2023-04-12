a1ffdc9c9288914481a882becc7f36fd1a4cc17c
Nanna Lea <nannalea@MacBook-Pro-tilhrende-Nanna-4.local>
2023-04-12 13:41:20 +0200

    Added Address tests.

--- index.php
+++ index.php
@@ -17,3 +17,37 @@ require_once 'src/Town.php';
 echo '<pre>';
 $fakeInfo = new FakeInfo;
 print_r($fakeInfo->getAddress());
+
+echo "\n";
+echo "\n";
+echo "Print:";
+echo "\n";
+print_r($fakeInfo->getAddress()["address"]);
+
+echo "\n";
+echo "\n";
+echo "Print street:";
+echo "\n";
+print_r($fakeInfo->getAddress()["address"]["street"]);
+
+echo "\n";
+echo "\n";
+echo "Print keys:";
+echo "\n";
+print_r(array_keys($fakeInfo->getAddress()["address"]));
+
+echo "\n";
+echo "\n";
+echo "Print values:";
+echo "\n";
+print_r(array_values($fakeInfo->getAddress()["address"]));
+
+echo "\n";
+echo "\n";
+
+$array = $fakeInfo->getAddress()["address"];
+if (array_key_exists('street', $array)) {
+    echo "The 'street' key is in the array";
+} else {
+        echo "The 'street' key is NOT in the array";
+    }
